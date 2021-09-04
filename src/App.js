0614e928cf911bcd6214094e7da166c09228d68e
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-04 19:14:16 +0530

    Added e2e tests

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import { Typography } from '@material-ui/core';
         <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
         </Typography>
+        <div data-test='book-list'>
+          <div className='book-item'>
+            <h2 className='title'>Refactoring</h2>
+          </div>
+          <div className='book-item'>
+            <h2 className='title'>Domain-driven design</h2>
+          </div>
+        </div>
       </div>
     );
  }
