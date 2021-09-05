7477948681c54df73270ad750ba61fb1671b9a04
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 22:08:01 +0530

    More unit test

--- src/BookDetail/BookDetail.js
+++ src/BookDetail/BookDetail.js
@@ -3,9 +3,13 @@ import React from 'react';
 const BookDetail = ({book}) => { 
     return (<div className='detail'>
         <h2 className='book-title'>{book.name}</h2> 
-        <p className='book-description'>{book.description}</p>
+        <p className='book-description'>{getDescriptionFor(book)}</p>
     </div>
     )
 };
 
+const getDescriptionFor = (book) => {
+    return book.description ? book.description : book.name;
+}
+  
 export default BookDetail;
