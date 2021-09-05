22646266e175bc73f1e0982471c0fbfccb73b5c4
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 19:08:35 +0530

    Added BooksDetail

--- cypress/cypress/integration/ui/bookish.spec.js
+++ cypress/cypress/integration/ui/bookish.spec.js
@@ -46,6 +46,7 @@ describe('Bookish application', () => {
     cy.visit('http://localhost:3000/');
     cy.get('div.book-item').contains('View Details').eq(0).click();
     cy.url().should('include', '/books/1');
+    cy.get('h2.book-title').contains('Refactoring');
   });
   
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import BookListContainer from './BookListContainer';
+import BookDetailContainer from './BookDetailContainer';
 
  const App = () => {
     return (
@@ -8,7 +10,11 @@ import BookListContainer from './BookListContainer';
         <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
         </Typography>
-        <BookListContainer/>
+        <Switch>
+          <Route exact path='/' component={BookListContainer}/>
+          <Route path='/books/:id' component={BookDetailContainer}/>
+        </Switch>
+        
       </div>
     );
  }
