27bc12ba15cb51a12efc23a18cfeb6f930ec9540
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 11:50:58 +0530

    Added useStates for loading & error

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import BookList from './BookList';
 
  const App = () => {
     const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
     useEffect(() => {
       const fetchBooks = async () => {
-        const res = await axios.get('http://localhost:8080/books');
-        setBooks(res.data);
-        };
-        fetchBooks();
+        setError(false);
+        setLoading(true);
+        try {
+          const res = await axios.get('http://localhost:8080/books');
+          setBooks(res.data);
+        } catch(e) {
+          setError(true);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchBooks();
     }, []);
 
     return (
