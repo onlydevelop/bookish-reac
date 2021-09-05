512f7ad1714201db2a56a27bdd78c6e373b41847
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 19:25:05 +0530

    Refactor: Extracted BookDetail component

--- src/BookDetailContainer.js
+++ src/BookDetailContainer.js
@@ -1,10 +1,12 @@
+import { BookmarkBorder, BookmarkBorderOutlined } from '@material-ui/icons';
 import React from 'react';
+import BookDetail from './BookDetail';
 import { useRemoteService } from './hook';
 
 const BookDetailContainer = ({match}) => { 
     const {data} = useRemoteService(`http://localhost:8080/books/${match.params.id}`, {});
     return (
-    <h2 className='book-title'>{data.name}</h2>
+    <BookDetail book={data}/>
     )
 };
 
