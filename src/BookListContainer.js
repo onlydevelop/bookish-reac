d9d72e5f9ec5fdc4fe98f432fc2dcd7c1c8c28ff
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 12:05:07 +0530

    Refactor: Extracted useRemoteService hook

--- src/BookListContainer.js
+++ src/BookListContainer.js
@@ -1,29 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 import BookList from './BookList';
+import { useRemoteService } from './hook';
 
 const BookListContainer = () => {
-    const [books, setBooks] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    useEffect(() => {
-      const fetchBooks = async () => {
-        setError(false);
-        setLoading(true);
-        try {
-          const res = await axios.get('http://localhost:8080/books');
-          setBooks(res.data);
-        } catch(e) {
-          setError(true);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchBooks();
-    }, []);
-
-    return <BookList books={books}/>
+    const {data, loading, error} = useRemoteService ([]);
+    return <BookList books={data} loading={loading} error={error}/>
 }
 
 export default BookListContainer;
