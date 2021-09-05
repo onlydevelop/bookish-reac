02bbe84a8f6d337bd43c0a871a648006d411f92b
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 11:56:30 +0530

    Refactor: Extract BookListContainer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Typography } from '@material-ui/core';
-import axios from 'axios';
-import BookList from './BookList';
+import BookListContainer from './BookListContainer';
 
  const App = () => {
-    const [books, setBooks] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-    useEffect(() => {
-      const fetchBooks = async () => {
-        setError(false);
-        setLoading(true);
-        try {
-          const res = await axios.get('http://localhost:8080/books');
-          setBooks(res.data);
-        } catch(e) {
-          setError(true);
-        } finally {
-          setLoading(false);
-        }
-      };
-      fetchBooks();
-    }, []);
-
     return (
       <div>
         <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
         </Typography>
-        <BookList books={books}/>
+        <BookListContainer/>
       </div>
     );
  }
---
+++ src/BookListContainer.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import BookList from './BookList';
+
+const BookListContainer = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+      const fetchBooks = async () => {
+        setError(false);
+        setLoading(true);
+        try {
+          const res = await axios.get('http://localhost:8080/books');
+          setBooks(res.data);
+        } catch(e) {
+          setError(true);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchBooks();
+    }, []);
+
+    return <BookList books={books}/>
+}
+
+export default BookListContainer;
