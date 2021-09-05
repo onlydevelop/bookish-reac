a3baa0d565410a28d1d50b4986eb3b7be3ac528a
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-05 11:18:54 +0530

    Added json server

--- cypress/cypress/integration/ui/bookish.spec.js
+++ cypress/cypress/integration/ui/bookish.spec.js
@@ -1,4 +1,32 @@
+import axios from 'axios';
+
 describe('Bookish application', () => {
+  before(() => {
+    return axios
+      .delete('http://localhost:8080/books?_cleanup=true')
+      .catch((err) => err);
+  });
+
+  beforeEach(() => {
+    const books = [
+      { 'name': 'Refactoring', 'id': 1 },
+      { 'name': 'Domain-driven design', 'id': 2 }
+    ];
+  
+    return books.map(item =>
+      axios.post('http://localhost:8080/books', item,
+        { headers: { 'Content-Type': 'application/json' } }
+      )
+    );
+  })
+  
+  afterEach(() => {
+    return axios
+      .delete('http://localhost:8080/books?_cleanup=true')
+      .catch(err => err);
+  })
+
+  
   it('Visits the bookish', () => {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish')
