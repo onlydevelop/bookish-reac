d6056831e43b843bb06bf56747c631983c466ec1
Dipanjan Bhowmik <onlydevelop@gmail.com>
2021-09-06 11:14:21 +0530

    Added onSearch

--- src/BookList/BookListContainer.js
+++ src/BookList/BookListContainer.js
@@ -1,19 +1,20 @@
 import BookList from './BookList';
 import { useRemoteService } from '../hook';
-import { TextField } from '@material-ui/core';
 import { useState, useEffect } from 'react';
+import SearchBox from '../SearchBox';
 
 const BookListContainer = () => {
     const [term, setTerm] = useState('');
     const {data, loading, error, setUrl} = useRemoteService ('http://localhost:8080/books', []);
     
+    const onSearch = (event) => setTerm(event.target.value);
 
     useEffect(() => {
         setUrl(`http://localhost:8080/books?q=${term}`)
       }, [term]);
     
     return (<div>
-        <TextField label='Search' value={term} data-test='search' onChange={(e) => setTerm(e.target.value)} margin='normal' variant='outlined'/>
+        <SearchBox term={term} onSearch={onSearch}/>
         <BookList books={data} loading={loading} error={error}/>
     </div>
     );
