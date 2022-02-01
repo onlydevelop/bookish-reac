4b4f95bfa5a0e4684152e683299ec3844f674064
Dipanjan Bhowmik <onlydevelop@gmail.com>
2022-02-01 11:40:44 +0530

    added redux

---
+++ redux/actions.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const setSearchTerm = (term) => {
+    return { type: 'SET_SEARCH_TERM', term }
+}
+
+export const fetchBooks = (term) => {
+  return (dispatch) => {
+    dispatch({type: 'FETCH_BOOKS_PENDING'})
+    return axios.get(`http://localhost:8080/books?q=${term}`)
+    .then((res) => {
+      dispatch({type: 'FETCH_BOOKS_SUCCESS', books: res.data})
+    }).catch((err) => {
+      dispatch({type: 'FETCH_BOOKS_FAILED', err: err.message})
+    })
+  }
+};
+
+  
